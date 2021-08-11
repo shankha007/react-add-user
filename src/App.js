f2d4ee33c69cd8434279b03f1cf966f06903099e
Shankha <ribhumaster@gmail.com>
2021-08-11 17:51:24 +0530

    added validation and error modal

--- src/App.js
+++ src/App.js
@@ -1,10 +1,24 @@
-import React from 'react';
+import React, { useState } from "react";
 
+import AddUser from "./components/Users/AddUser";
+import UsersList from "./components/Users/UsersList";
+
+const INITIAL_USERS = [
+  { name: "Max", age: 27, id: 'u1' },
+  { name: "Alice", age: 25, id: 'u2' },
+];
 
 function App() {
+  const [users, setUsers] = useState(INITIAL_USERS);
+
+  const pushNewUser = (name, age) => {
+    setUsers((prevUsers) => [{name, age, id: Math.random().toString()}, ...prevUsers]);
+  };
+
   return (
     <div>
-
+      <AddUser addUser={pushNewUser} />
+      <UsersList users={users} />
     </div>
   );
 }
